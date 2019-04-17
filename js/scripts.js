0c0f6633b443f19f10fd4e5a0810e1e71f55cfb8
rakhat5555 <rakhat5555@gmail.com>
2019-04-17 14:53:04 -0700

    add for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 var pingPong = function(number) {
-  if (number % 15 === 0){
-  return "PingPong";
-} else if (number % 5 === 0) {
-  return "Pong";
-} else if (number % 3 === 0) {
-  return "Ping";
-} else {
-  return number;
-}
+  var output=[];
+  for(var i=1; i<=number; i++) {
+    if (i % 15 === 0){
+      output.push("PingPong");
+    } else if (i % 5 === 0) {
+      output.push("Pong");
+    } else if (i % 3 === 0) {
+      output.push("Ping");
+    } else {
+      output.push(i);
+    }
+  }
+  return output;
 };
 
-
 $(document).ready(function() {
-  // debugger;
   $("form").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
     var result = pingPong(number);
-    $(".result").append("<li>" + result + "</li>");
-    $("#result").show();
+    result.forEach(function(total) {
+      $(".result").append("<li>" + total + "</li>");
+    });
   });
 });
